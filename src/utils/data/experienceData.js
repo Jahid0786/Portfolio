f0ee4fd1661fe3beaf021b5aabce18bc8e10f5a4
saqeebkhan8271@gmail.com <saqeebkhan8271@gmail.com>
2025-03-22 12:01:31 +0530

    Initializing gh-pages

---
+++ src/utils/data/experienceData.js
@@ -0,0 +1,20 @@
+export const experiences = [
+    {
+      id: 1,
+      title: 'Android Developer',
+      company: "BrightCode Software Services Private Ltd.",
+      duration: "(Sep 2022 - Apr 2023)"
+    },
+    {
+      id: 2,
+      title: "Front-end Developer",
+      company: "Freelance",
+      duration: "(Nov 2024 - Jan 2025)"
+    },
+    {
+      id: 3,
+      title: "Software Devleper(Internship)",
+      company: "Briztech InfoSystems Pvt Ltd                                                                                                                                                                                                    .",
+      duration: "(July 2023 - Nov 2023)"
+    }
+  ]
---
+++ src/utils/data/personalData.js
@@ -0,0 +1,17 @@
+export const personalData = {
+    name: "JAHID ANSARI",
+    profile: '/profile.png',
+    designation: "Software Developer",
+    description: "My name is JAHID ANSARI. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and Android Development and love to make the app & web more open to the world. My core skill is based on Android  and I love to do most of the things using Java / XML. I am available for any kind of job opportunity that suits my skills and interests.",
+    email: 'jahid8434@gmail.com',
+    phone: '+91 8434863638',
+    address: ' Karam Tola Kothar, Ramgarh , Jharkhand 829122 ',
+    github: 'https://github.com/Jahid0786',
+    facebook: '',
+    linkedIn: '',
+    twitter: '',
+    stackOverflow: '',
+    leetcode: "",
+    devUsername: "jahid0786",
+    resume: "https://drive.google.com/file/d/1aGXe0zfOUco9jVe8a0bYErF8UOw0-LXZ/view?usp=sharing "
+  }
